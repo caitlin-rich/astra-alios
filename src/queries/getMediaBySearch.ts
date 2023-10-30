1e7a817e1b6a3fb6feb3f21c3ee27394c1122942
caitlin-rich <caitlinboagrich@gmail.com>
2023-10-30 10:52:12 -0400

    moves api key

--- src/queries/getMediaBySearch.ts
+++ src/queries/getMediaBySearch.ts
@@ -1,11 +1,13 @@
 const fetch = require('node-fetch');
+import API_KEY
+//I know this isn't right but I am too tired to fix it immediately.
 
 const url = 'https://api.themoviedb.org/3/search/tv?include_adult=false&language=en-US&page=1';
 const options = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkODMwMjYyNjRhMzRiNzYxMDE0MjgxMjllMTIyMDhhZiIsInN1YiI6IjY0ZmUzNjVhMmRmZmQ4MDBhZGI2Nzk2YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.skFkc9-0tU6utxbezR4vGOZgm54xqmZo9dWU_uDEeGQ'
+    Authorization: API_KEY
   }
 };
 
