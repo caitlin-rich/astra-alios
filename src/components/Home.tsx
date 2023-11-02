fe48ca11a874ff426eee1d0ab8aefd634d939d9d
caitlin-rich <caitlinboagrich@gmail.com>
2023-11-02 15:35:02 -0400

    updates api key variable and tests success

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,29 @@
+import { Button } from "@material-ui/core";
+import getMediaBySearch from "../queries/getMediaBySearch";
+import { useState } from "react";
 
 export default function Home() {
+
+  const [media, setMedia] = useState([])
+
+  const handleClick = async () => {
+    const response = await getMediaBySearch('stargate sg-1')
+    response && setMedia(response)
+  }
+
+  //we just get the entire array back regardless of if it finds the right one or not
+  //then if array.length > 1 we can list them all and have user click on correct one
+  //if array length is one then we can skip that step
+  //either way that sets the hook with the show ID and we can move onto the next step
+  //obviously this isn't ideal UX but it's a start
+  //unless material UI has a box that'll do the work for me
+
+
   return (
     <div>
         <h1>AD ASTRA PER ALIOS</h1>
-        <input />
+        <Button onClick={handleClick}>Testing API Call Round One</Button>
+        
     </div>
   )
 }
--- src/queries/getMediaBySearch.ts
+++ src/queries/getMediaBySearch.ts
@@ -1,17 +1,23 @@
-const fetch = require('node-fetch');
-import API_KEY
-//I know this isn't right but I am too tired to fix it immediately.
+const {REACT_APP_TMDB_API_KEY} = process.env
 
-const url = 'https://api.themoviedb.org/3/search/tv?include_adult=false&language=en-US&page=1';
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: API_KEY
-  }
-};
+const getMediaBySearch = (searchTerm: string) => {
 
-fetch(url, options)
+  const url = `https://api.themoviedb.org/3/search/tv?query=${searchTerm}&include_adult=false&language=en-US&page=1`;
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${REACT_APP_TMDB_API_KEY}`
+    }
+  };
+
+  const response = fetch(url, options)
   .then(res => res.json())
   .then(json => console.log(json))
   .catch(err => console.error('error:' + err));
+
+  console.log('testing', response)
+  return response
+}
+
+export default getMediaBySearch
